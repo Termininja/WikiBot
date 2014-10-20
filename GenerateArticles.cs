a421b07ee7d3eea526f55aac817c86792a66f871
Termininja <termininja@gmail.com>
2014-10-20 14:24:06 +0300

    Wikipedia

--- GenerateArticles.cs
+++ GenerateArticles.cs
@@ -1,39 +1,42 @@
 namespace WikiBot
 {
+    using System;
     using System.Data;
+    using System.Threading;
 
     using ArtOfTest.WebAii.Controls.HtmlControls;
     using ArtOfTest.WebAii.Core;
     using ArtOfTest.WebAii.TestTemplates;
     using ClosedXML.Excel;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System;
 
     [TestClass]
     public class GenerateArticles : BaseTest
     {
         private const string Url = "https://tools.wmflabs.org/templatetiger/tt-table4.php?template=taxobox&lang=enwiki";
+        private const string SheetName = "Taxobox";
         private const string FilePath = "../../../Templates/";
-        private const int NumberOfLines = 10;
-        private const int Period = 100;
+        private const int Lines = 10;
+        private const int Period = 10000;
+        private const int SleepTime = 10000;
 
         public TestContext TestContext { get; set; }
 
-        [TestInitialize()]
-        public void MyTestInitialize()
+        [TestInitialize]
+        public void TestInitialize()
         {
-            Initialize(false, this.TestContext.TestLogsDir, new TestContextWriteLine(this.TestContext.WriteLine));
-            SetTestMethod(this, (string)TestContext.Properties["TestName"]);
+            this.Initialize(false, this.TestContext.TestLogsDir, new TestContextWriteLine(this.TestContext.WriteLine));
+            this.SetTestMethod(this, (string)TestContext.Properties["TestName"]);
         }
 
-        [TestCleanup()]
-        public void MyTestCleanup()
+        [TestCleanup]
+        public void TestCleanup()
         {
             this.CleanUp();
         }
 
-        [ClassCleanup()]
-        public static void MyClassCleanup()
+        [ClassCleanup]
+        public void ClassCleanup()
         {
             ShutDown();
         }
@@ -42,45 +45,31 @@ namespace WikiBot
         public void ExportEnTaxoboxTemplateToExcel()
         {
             Manager.LaunchNewBrowser(BrowserType.InternetExplorer);
-            ActiveBrowser.NavigateTo(Url + "&limit=" + NumberOfLines);
+            ActiveBrowser.NavigateTo(Url + "&limit=" + Lines);
 
             DataTable dataTable = new DataTable();
             dataTable.Clear();
 
-            try
+            for (int count = 1; count < int.MaxValue; count++)
             {
-                int count = 0;
-                while (true)
+                this.ExtractTable(dataTable);
+
+                if ((count * Lines) % Period == 0)
                 {
-                    count++;
-                    if (ActiveBrowser.AutoWaitUntilReady)
-                    {
-                        ExtractTable(dataTable);
-                    }
+                    int realCount = count * Lines;
+                    this.ExportToExcel(dataTable, FilePath + "Templates (" + (realCount - Period) + "-" + realCount + ").xlsx");
+                    dataTable.Clear();
 
-                    if ((count * NumberOfLines) % Period == 0)
-                    {
-                        int realCount = count * NumberOfLines;
-                        ExportToExcel(dataTable, "Taxobox", FilePath + "Templates (" + (realCount - Period) + "-" + realCount + ").xlsx");
-                        dataTable.Clear();
-                    }
-
-                    // Go to the next page
-                    var next = Find.ByContent<HtmlAnchor>(">>>");
-                    if (next != null) next.Click();
-                    else break;
+                    Thread.Sleep(SleepTime);
                 }
 
-                ExportToExcel(dataTable, "Taxobox", FilePath + "Templates.xlsx");
+                // Go to the next page
+                var next = Find.ByContent<HtmlAnchor>(">>>");
+                if (next != null) next.Click();
+                else break;
             }
-            catch (Exception ex)
-            {
-                DataRow dataRow = dataTable.NewRow();
-                dataRow["link"] = "Error: " + ex.ToString();
-                dataTable.Rows.Add(dataRow);
 
-                ExportToExcel(dataTable, "Taxobox", FilePath + "Templates (with Errors).xlsx");
-            }
+            this.ExportToExcel(dataTable, FilePath + "Templates.xlsx");
         }
 
         // Take all table's data from the current web page
@@ -90,7 +79,7 @@ namespace WikiBot
             var cells = table.Rows[0].Cells;
             for (int c = 0; c < cells.Count; c++)
             {
-                var currentColumnName = cells[c].InnerText == "" ? "link" : cells[c].InnerText;
+                var currentColumnName = cells[c].InnerText == string.Empty ? "link" : cells[c].InnerText;
                 if (!dataTable.Columns.Contains(currentColumnName))
                 {
                     dataTable.Columns.Add(currentColumnName);
@@ -103,7 +92,7 @@ namespace WikiBot
                 DataRow dataRow = dataTable.NewRow();
                 for (int c = 0; c < cells.Count; c++)
                 {
-                    dataRow[cells[c].InnerText == "" ? "link" : cells[c].InnerText] = rows[r][c].InnerText;
+                    dataRow[cells[c].InnerText == string.Empty ? "link" : cells[c].InnerText] = rows[r][c].InnerText;
                 }
 
                 dataTable.Rows.Add(dataRow);
@@ -111,10 +100,10 @@ namespace WikiBot
         }
 
         // Export all data to an Excel file
-        private static void ExportToExcel(DataTable dataTable, string sheetName, string filePath)
+        private void ExportToExcel(DataTable dataTable, string filePath)
         {
             XLWorkbook workbook = new XLWorkbook();
-            workbook.Worksheets.Add(dataTable, sheetName);
+            workbook.Worksheets.Add(dataTable, SheetName);
             workbook.SaveAs(filePath);
         }
     }
