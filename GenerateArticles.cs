c8f10b44f4b37abb2d94390f9ff29805d968c334
Termininja <termininja@gmail.com>
2014-10-20 22:05:27 +0300

    Wikipedia

--- GenerateArticles.cs
+++ GenerateArticles.cs
@@ -1,8 +1,8 @@
 namespace WikiBot
 {
     using System;
+    using System.Collections.Generic;
     using System.Data;
-    using System.Threading;
 
     using ArtOfTest.WebAii.Controls.HtmlControls;
     using ArtOfTest.WebAii.Core;
@@ -17,11 +17,9 @@ namespace WikiBot
         private const string SheetName = "Taxobox";
         private const string FilePath = "../../../Templates/";
 
-        private const int Start = 0;
-        private const int End = int.MaxValue;
-        private const int Lines = 100;
+        private const int Start = 60000;
+        private const int Lines = 2000;
         private const int Period = 10000;
-        private const int SleepTime = 10000;
 
         public TestContext TestContext { get; set; }
         public DataTable DataTable { get; set; }
@@ -52,24 +50,29 @@ namespace WikiBot
             ActiveBrowser.NavigateTo(Url + "&offset=" + Start + "&limit=" + Lines);
             this.CreateTable();
 
-            for (int count = Start + 1; count < End; count++)
+            try
             {
-                this.ExtractTable();
-
-                if ((((count - Start) * Lines) + Start) % Period == 0)
+                for (int count = Start + 1; count < int.MaxValue; count++)
                 {
-                    int realCount = (count - Start) * Lines + Start;
-                    this.ExportToExcel(FilePath + "Templates (" + (realCount - Period + 1) + "-" + realCount + ").xlsx");
-                    this.CreateTable();
+                    this.ExtractTable();
+
+                    if ((((count - Start) * Lines) + Start) % Period == 0)
+                    {
+                        int realCount = (count - Start) * Lines + Start;
+                        this.ExportToExcel(FilePath + "Templates (" + (realCount - Period + 1) + "-" + realCount + ").xlsx");
+                        this.CreateTable();
+                    }
+
+                    // Go to the next page
+                    var next = Find.ByContent<HtmlAnchor>(">>>");
+                    if (next != null) next.Click();
+                    else break;
                 }
-
-                // Go to the next page
-                var next = Find.ByContent<HtmlAnchor>(">>>");
-                if (next != null) next.Click();
-                else break;
             }
-
-            this.ExportToExcel(FilePath + "Templates.xlsx");
+            finally
+            {
+                this.ExportToExcel(FilePath + "Templates.xlsx");
+            }
         }
 
         // Create new empty data table
@@ -77,7 +80,6 @@ namespace WikiBot
         {
             this.DataTable = new DataTable();
             this.DataTable.Clear();
-            Thread.Sleep(SleepTime);
         }
 
         // Take all table's data from the current web page
@@ -87,7 +89,7 @@ namespace WikiBot
             var cells = table.Rows[0].Cells;
             for (int c = 0; c < cells.Count; c++)
             {
-                var currentColumnName = cells[c].InnerText == string.Empty ? "link" : cells[c].InnerText;
+                var currentColumnName = ReadCell(cells, c);
                 if (!this.DataTable.Columns.Contains(currentColumnName))
                 {
                     this.DataTable.Columns.Add(currentColumnName);
@@ -100,11 +102,34 @@ namespace WikiBot
                 DataRow dataRow = this.DataTable.NewRow();
                 for (int c = 0; c < cells.Count; c++)
                 {
-                    dataRow[cells[c].InnerText == string.Empty ? "link" : cells[c].InnerText] = rows[r][c].InnerText;
+                    if (c < rows[r].Cells.Count)
+                    {
+                        dataRow[ReadCell(cells, c)] = rows[r][c].InnerText;
+                    }
                 }
 
                 this.DataTable.Rows.Add(dataRow);
             }
+        }
+
+        // Read the value from the current cell
+        private static string ReadCell(List<HtmlTableCell> cells, int currentCell)
+        {
+            string cellValue = string.Empty;
+
+            switch (cells[currentCell].InnerText)
+            {
+                case "": cellValue = "link"; break;
+                case "0": cellValue = "zero"; break;
+                case "1": cellValue = "one"; break;
+                case "2": cellValue = "two"; break;
+                case "3": cellValue = "three"; break;
+                case "4": cellValue = "four"; break;
+                case "5": cellValue = "five"; break;
+                default: cellValue = cells[currentCell].InnerText; break;
+            }
+
+            return cellValue;
         }
 
         // Export all data to an Excel file
