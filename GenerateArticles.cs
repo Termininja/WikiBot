eb98958a15ed21a2e7a13692fa04e94957e1b871
Termininja <termininja@gmail.com>
2014-10-20 13:53:28 +0300

    Wikipedia

--- GenerateArticles.cs
+++ GenerateArticles.cs
@@ -7,10 +7,16 @@ namespace WikiBot
     using ArtOfTest.WebAii.TestTemplates;
     using ClosedXML.Excel;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
 
     [TestClass]
     public class GenerateArticles : BaseTest
     {
+        private const string Url = "https://tools.wmflabs.org/templatetiger/tt-table4.php?template=taxobox&lang=enwiki";
+        private const string FilePath = "../../../Templates/";
+        private const int NumberOfLines = 10;
+        private const int Period = 100;
+
         public TestContext TestContext { get; set; }
 
         [TestInitialize()]
@@ -36,24 +42,45 @@ namespace WikiBot
         public void ExportEnTaxoboxTemplateToExcel()
         {
             Manager.LaunchNewBrowser(BrowserType.InternetExplorer);
-            ActiveBrowser.NavigateTo("https://tools.wmflabs.org/templatetiger/tt-table4.php?template=taxobox&lang=enwiki&where=&is=&limit=10");
+            ActiveBrowser.NavigateTo(Url + "&limit=" + NumberOfLines);
 
             DataTable dataTable = new DataTable();
             dataTable.Clear();
 
-            int k = 0;
-            while (k < 10)
+            try
             {
-                k++;
-                ExtractTable(dataTable);
+                int count = 0;
+                while (true)
+                {
+                    count++;
+                    if (ActiveBrowser.AutoWaitUntilReady)
+                    {
+                        ExtractTable(dataTable);
+                    }
 
-                // Go to the next page
-                var next = Find.ByContent<HtmlAnchor>(">>>");
-                if (next != null) next.Click();
-                else break;
+                    if ((count * NumberOfLines) % Period == 0)
+                    {
+                        int realCount = count * NumberOfLines;
+                        ExportToExcel(dataTable, "Taxobox", FilePath + "Templates (" + (realCount - Period) + "-" + realCount + ").xlsx");
+                        dataTable.Clear();
+                    }
+
+                    // Go to the next page
+                    var next = Find.ByContent<HtmlAnchor>(">>>");
+                    if (next != null) next.Click();
+                    else break;
+                }
+
+                ExportToExcel(dataTable, "Taxobox", FilePath + "Templates.xlsx");
             }
+            catch (Exception ex)
+            {
+                DataRow dataRow = dataTable.NewRow();
+                dataRow["link"] = "Error: " + ex.ToString();
+                dataTable.Rows.Add(dataRow);
 
-            ExportToExcel(dataTable, "Taxobox", "TaxoboxTemplates.xlsx");
+                ExportToExcel(dataTable, "Taxobox", FilePath + "Templates (with Errors).xlsx");
+            }
         }
 
         // Take all table's data from the current web page
