46f3f86847744d8d0d2e1b4b264b51f6da9272b5
Termininja <termininja@mail.bg>
2015-11-12 19:12:49 +0200

    Wiki attributes

---
+++ WikiBot/Wiki/Attributes/TypeAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Wiki
+{
+    using System;
+
+    using Data.Enums;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class TypeAttribute : Attribute
+    {
+        public TypeAttribute(DataType dataType)
+        {
+            this.DataType = dataType;
+        }
+
+        public DataType DataType { get; private set; }
+    }
+}
---
+++ WikiBot/Wiki/Attributes/UrlAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Wiki
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class UrlAttribute : Attribute
+    {
+        public UrlAttribute(string url)
+        {
+            this.Url = url;
+        }
+
+        public string Url { get; private set; }
+    }
+}
